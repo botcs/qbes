815f6a8787d8292961bb33734e0aeb0bc076ba5a
Botos Csaba <botos.official@gmail.com>
2022-06-14 09:17:49 -0700

    eval on full imagenet training set

--- imagenet-exp/launch-jupyter.sh
+++ imagenet-exp/launch-jupyter.sh
@@ -3,7 +3,6 @@
 #SBATCH -c 12
 #SBATCH --gres=gpu:8
 #SBATCH --qos=normal
-#SBATCH --time=2-00:00:00
 #SBATCH -o /home/cbotos/github/qbes/imagenet-exp/JUPYTER_OUT/%J.out # STDOUT
 #SBATCH -e /home/cbotos/github/qbes/imagenet-exp/JUPYTER_OUT/%J.err # STDERR
 eval "$(conda shell.bash hook)"
